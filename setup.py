52a750a849dc3571c20fe360f6b1b37a6591581d
Dmitri Babaev <dmitri.babaev@gmail.com>
2019-08-31 01:20:55 +0300

    requrements

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 import common
 
+# Running Elasticsearch server is required
+# docker run -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:5.2.1
+
 common.index_ck_12_conecpts_v8()
